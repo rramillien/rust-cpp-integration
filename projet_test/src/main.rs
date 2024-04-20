ff8f6cb31926056040c75a7202ea79c998ea97d9
Régis Ramillien <r.ramillien@tritonit.tech>
2024-04-21 00:05:50 +0200

    Buffer sent from main to lib and read from main.

--- projet_test/src/main.rs
+++ projet_test/src/main.rs
@@ -1,17 +1,12 @@
+use oscillator_module::Oscillator;
 
-use oscillateur_lib::Oscillateur;
 fn main() {
-    let mut osc = Oscillateur::new(440.0, 0.0, 44100.0, 1024); // Crée un oscillateur avec un buffer de taille 1024
-    let mut x = 0; // Compteur pour simuler l'axe x
-
-    // Boucle infinie pour générer et afficher les échantillons
-    loop {
-        let sample = osc.generate_and_get_next_sample();
-        println!("Sample {}: {}", x, sample);
-
-        x += 1;  // Incrémentation de l'index après chaque échantillon
-
-        // Pause pour simuler un taux d'échantillonnage en temps réel et éviter de saturer la sortie
-        std::thread::sleep(std::time::Duration::from_millis(10));
+    let mut buffer = [0.0; 255];
+    let mut obj = Oscillator::new(440.0, 0.0, 44100.0, &mut buffer);
+    for i in 0..9 {
+        let buffer = obj.process();
+        for j in 0..buffer.len() {
+            println!("Loop {}: subloop: {}: sample {}", i, j, buffer[j]);
+        }
     }
 }
